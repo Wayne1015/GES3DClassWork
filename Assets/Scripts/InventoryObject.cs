cd494f6b36b3e3638346c82dba5d553522df7969
unknown <Wayne.Thackston@iam.local>
2017-12-04 18:20:45 -0600

    InventoryMenustuff

---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour, IActivatable {
+    [SerializeField]
+    private string nameText;
+
+    [SerializeField]
+    private string DescriptionText;
+
+    private MeshRenderer meshRenderer;
+    private Collider collider;
+    private List<InventoryObject> playerInventory;
+    public string NameText
+    {
+        get
+        {
+            return nameText;
+        }
+    }
+
+    public void DoActivate()
+    {
+        playerInventory.Add(this);
+        meshRenderer.enabled = false;
+        collider.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        collider = GetComponent<Collider>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        playerInventory = FindObjectOfType<InventoryMenu>().PlayerInventory;
+	}
+	
+	
+}
